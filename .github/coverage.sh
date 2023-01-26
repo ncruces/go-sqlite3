44854a7495f705bc26b7bed410ae4c11f4fb17b1
Nuno Cruces <ncruces@users.noreply.github.com>
2023-01-26 10:10:13 +0000

    Coverage hook.

--- .github/coverage.sh
+++ .github/coverage.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-cd -P -- "$(dirname -- "$0")"
+SCRIPT_DIR="$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-go test ../... -coverprofile coverage.svg
-COVERAGE=$(go tool cover -func=coverage.svg | grep total: | grep -Eo '[0-9]+\.[0-9]+')
-echo $COVERAGE
+go test ./... -coverprofile "$SCRIPT_DIR/coverage.out"
+go tool cover -html="$SCRIPT_DIR/coverage.out" -o "$SCRIPT_DIR/coverage.html"
+COVERAGE=$(go tool cover -func="$SCRIPT_DIR/coverage.out" | grep total: | grep -Eo '[0-9]+\.[0-9]+')
+
+echo
+echo "Full coverage: $COVERAGE% of statements"
+
 COLOR=orange
 if (( $(echo "$COVERAGE <= 50" | bc -l) )) ; then
 	COLOR=red
 elif (( $(echo "$COVERAGE > 80" | bc -l) )); then
 	COLOR=green
 fi
-curl -s "https://img.shields.io/badge/coverage-$COVERAGE%25-$COLOR" > coverage.svg
+curl -s "https://img.shields.io/badge/coverage-$COVERAGE%25-$COLOR" > "$SCRIPT_DIR/coverage.svg"
+
+git add "$SCRIPT_DIR/coverage.html" "$SCRIPT_DIR/coverage.svg"
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Go Reference](https://pkg.go.dev/badge/image)](https://pkg.go.dev/github.com/ncruces/go-sqlite3)
 [![Go Report](https://goreportcard.com/badge/github.com/ncruces/go-sqlite3)](https://goreportcard.com/report/github.com/ncruces/go-sqlite3)
-![Go Coverage](.github/coverage.svg)
+[![Go Coverage](.github/coverage.svg)](https://raw.githack.com/ncruces/go-sqlite3/main/.github/coverage.html)
 
 ⚠️ CAUTION ⚠️
 
