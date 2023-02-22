75f2644b0e3f03404b07ea7d617f7cd87968b654
Nuno Cruces <ncruces@users.noreply.github.com>
2023-02-22 20:08:50 +0000

    SQLite 3.41.0.

--- sqlite3/download.sh
+++ sqlite3/download.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 cd -P -- "$(dirname -- "$0")"
 
 if [ ! -f "sqlite3.c" ]; then
-	url="https://www.sqlite.org/2022/sqlite-amalgamation-3400100.zip"
+	url="https://sqlite.org/2023/sqlite-amalgamation-3410000.zip"
 	curl "$url" > sqlite.zip
 	unzip -d . sqlite.zip
 	mv sqlite-amalgamation-*/sqlite3* .
