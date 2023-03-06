1ebdc1aa930d35ff61d017a8d71b2cd9c9fc7dc5
Nuno Cruces <ncruces@users.noreply.github.com>
2023-03-06 17:51:25 +0000

    Towards shared modules: refactor.

--- module.go
+++ module.go
@@ -3,6 +3,7 @@ package sqlite3
 import (
 	"context"
 	"crypto/rand"
+	"math"
 	"os"
 	"runtime"
 	"strconv"
@@ -92,4 +93,41 @@ type module struct {
 	ctx context.Context
 	mem memory
 	api sqliteAPI
+}
+
+func (c *module) error(rc uint64, handle uint32, sql ...string) error {
+	if rc == _OK {
+		return nil
+	}
+
+	err := Error{code: rc}
+
+	if err.Code() == NOMEM || err.ExtendedCode() == IOERR_NOMEM {
+		panic(oomErr)
+	}
+
+	var r []uint64
+
+	r, _ = c.api.errstr.Call(c.ctx, rc)
+	if r != nil {
+		err.str = c.mem.readString(uint32(r[0]), _MAX_STRING)
+	}
+
+	r, _ = c.api.errmsg.Call(c.ctx, uint64(handle))
+	if r != nil {
+		err.msg = c.mem.readString(uint32(r[0]), _MAX_STRING)
+	}
+
+	if sql != nil {
+		r, _ = c.api.erroff.Call(c.ctx, uint64(handle))
+		if r != nil && r[0] != math.MaxUint32 {
+			err.sql = sql[0][r[0]:]
+		}
+	}
+
+	switch err.msg {
+	case err.str, "not an error":
+		err.msg = ""
+	}
+	return &err
 }
