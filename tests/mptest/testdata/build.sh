6ddd8302204fef0e4368e05d39b37f60667b07ab
Nuno Cruces <ncruces@users.noreply.github.com>
2023-03-07 03:51:07 +0000

    Pass mptest crash.

--- tests/mptest/testdata/build.sh
+++ tests/mptest/testdata/build.sh
@@ -19,6 +19,7 @@ zig cc --target=wasm32-wasi -flto -g0 -Os \
 	-mbulk-memory -mreference-types \
 	-mnontrapping-fptoint -msign-ext \
 	-D_HAVE_SQLITE_CONFIG_H \
+	-DSQLITE_DEFAULT_SYNCHRONOUS=0 \
 	-DSQLITE_DEFAULT_LOCKING_MODE=0 \
 	-DHAVE_USLEEP -DSQLITE_NO_SYNC \
 	-DSQLITE_THREADSAFE=0 -DSQLITE_OMIT_LOAD_EXTENSION \
