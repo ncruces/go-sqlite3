b801c3fab63ea358149d1658ea629e21526a0684
Nuno Cruces <ncruces@users.noreply.github.com>
2024-02-14 00:26:14 +0000

    Try again.

--- ext/steampipe/steampipe.go
+++ ext/steampipe/steampipe.go
@@ -4,16 +4,33 @@ import (
 	"strings"
 
 	"github.com/ncruces/go-sqlite3"
+	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 )
 
-func Register(db *sqlite3.Conn, plugin *plugin.Plugin) error {
-	for name := range plugin.TableMap {
+func Register(db *sqlite3.Conn, name string, fn plugin.PluginFunc) error {
+	server := plugin.Server(&plugin.ServeOpts{
+		PluginName: name,
+		PluginFunc: fn,
+	})
+
+	_, err := server.SetAllConnectionConfigs(&proto.SetAllConnectionConfigsRequest{
+		Configs: []*proto.ConnectionConfig{{Connection: name}},
+	})
+	if err != nil {
+		return err
+	}
+
+	res, err := server.GetSchema(&proto.GetSchemaRequest{})
+	if err != nil {
+		return err
+	}
+
+	schema := res.Schema.Schema
+	for name := range schema {
 		err := sqlite3.CreateModule[*table](db, name, nil,
 			func(db *sqlite3.Conn, _, _, name string, _ ...string) (*table, error) {
-				table := table{
-					Table: plugin.TableMap[name],
-				}
+				table := table{TableSchema: schema[name]}
 
 				var sep string
 				var str strings.Builder
@@ -40,7 +57,7 @@ func Register(db *sqlite3.Conn, plugin *plugin.Plugin) error {
 }
 
 type table struct {
-	*plugin.Table
+	*proto.TableSchema
 }
 
 func (*table) BestIndex(idx *sqlite3.IndexInfo) error {
