fed9ce6e1c6d5ac11eda0a9d24bb4e16fc6def16
Nuno Cruces <ncruces@users.noreply.github.com>
2024-03-07 03:03:56 -0800

    Backport date from 3.46.

--- sqlite3/include.h
+++ sqlite3/include.h
@@ -5,7 +5,7 @@
 
 // https://github.com/JuliaLang/julia/blob/v1.9.4/src/julia.h#L67-L68
 #define container_of(ptr, type, member) \
-  ((type *)((char *)(ptr)-offsetof(type, member)))
+  ((type *)((char *)(ptr) - offsetof(type, member)))
 
 typedef void *go_handle;
 void go_destroy(go_handle);
--- sqlite3/time.c
+++ sqlite3/time.c
@@ -27,63 +27,10 @@ static int time_collation(void *pArg, int nKey1, const void *pKey1, int nKey2,
   return rc;
 }
 
-static void json_time_func(sqlite3_context *context, int argc,
-                           sqlite3_value **argv) {
-  DateTime x;
-  if (isDate(context, argc, argv, &x)) return;
-  if (x.tzSet && x.tz) {
-    x.iJD += x.tz * 60000;
-    if (!validJulianDay(x.iJD)) return;
-    x.validYMD = 0;
-    x.validHMS = 0;
-  }
-  computeYMD_HMS(&x);
-
-  sqlite3 *db = sqlite3_context_db_handle(context);
-  sqlite3_str *res = sqlite3_str_new(db);
-
-  sqlite3_str_appendf(res, "%04d-%02d-%02dT%02d:%02d:%02d",  //
-                      x.Y, x.M, x.D,                         //
-                      x.h, x.m, (int)(x.iJD / 1000 % 60));
-
-  if (x.useSubsec) {
-    int rem = x.iJD % 1000;
-    if (rem) {
-      sqlite3_str_appendchar(res, 1, '.');
-      sqlite3_str_appendchar(res, 1, '0' + rem / 100);
-      if ((rem %= 100)) {
-        sqlite3_str_appendchar(res, 1, '0' + rem / 10);
-        if ((rem %= 10)) {
-          sqlite3_str_appendchar(res, 1, '0' + rem);
-        }
-      }
-    }
-  }
-
-  if (x.tz) {
-    sqlite3_str_appendf(res, "%+03d:%02d", x.tz / 60, abs(x.tz) % 60);
-  } else {
-    sqlite3_str_appendchar(res, 1, 'Z');
-  }
-
-  int rc = sqlite3_str_errcode(res);
-  if (rc) {
-    sqlite3_result_error_code(context, rc);
-    return;
-  }
-
-  int n = sqlite3_str_length(res);
-  sqlite3_result_text(context, sqlite3_str_finish(res), n, sqlite3_free);
-}
-
 int sqlite3_time_init(sqlite3 *db, char **pzErrMsg,
                       const sqlite3_api_routines *pApi) {
   sqlite3_create_collation_v2(db, "time", SQLITE_UTF8, /*arg=*/NULL,
                               time_collation,
                               /*destroy=*/NULL);
-  sqlite3_create_function_v2(
-      db, "json_time", /*nArg=*/-1,
-      SQLITE_UTF8 | SQLITE_DETERMINISTIC | SQLITE_INNOCUOUS, /*arg=*/NULL,
-      json_time_func, /*step=*/NULL, /*final=*/NULL, /*destroy=*/NULL);
   return SQLITE_OK;
 }
