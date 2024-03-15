7bd31c34435a77203914e4d1ba1b36a42e224c4f
Nuno Cruces <ncruces@users.noreply.github.com>
2024-03-15 14:15:12 +0000

    Use strchrnul.

--- sqlite3/sqlite_cfg.h
+++ sqlite3/sqlite_cfg.h
@@ -9,16 +9,20 @@
 #define HAVE_INT16_T 1
 #define HAVE_INT32_T 1
 #define HAVE_INT64_T 1
+#define HAVE_INTPTR_T 1
 #define HAVE_UINT8_T 1
 #define HAVE_UINT16_T 1
 #define HAVE_UINT32_T 1
 #define HAVE_UINT64_T 1
+#define HAVE_UINTPTR_T 1
 #define HAVE_STDINT_H 1
 #define HAVE_INTTYPES_H 1
 
 #define HAVE_LOG2 1
 #define HAVE_LOG10 1
 #define HAVE_ISNAN 1
+
+#define HAVE_STRCHRNUL 1
 
 #define HAVE_USLEEP 1
 #define HAVE_NANOSLEEP 1
