e7f8311e2e0dc552790e2024e4c8200f63110266
Nuno Cruces <ncruces@users.noreply.github.com>
2024-06-10 00:24:15 +0100

    Fix readonly shared memory (see #94).

--- tests/wal_test.go
+++ tests/wal_test.go
@@ -1,11 +1,11 @@
 package tests
 
 import (
-	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/ncruces/go-sqlite3"
+	"github.com/ncruces/go-sqlite3/driver"
 	_ "github.com/ncruces/go-sqlite3/embed"
 	_ "github.com/ncruces/go-sqlite3/internal/testcfg"
 	"github.com/ncruces/go-sqlite3/vfs"
@@ -52,26 +52,55 @@ func TestWAL_readonly(t *testing.T) {
 	}
 	t.Parallel()
 
-	tmp := filepath.Join(t.TempDir(), "test.db")
-	err := os.WriteFile(tmp, walDB, 0666)
+	tmp := filepath.ToSlash(filepath.Join(t.TempDir(), "test.db"))
+
+	db1, err := driver.Open("file:"+tmp+"?_pragma=journal_mode(wal)&_txlock=immediate", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db1.Close()
+
+	db2, err := driver.Open("file:"+tmp+"?_pragma=journal_mode(wal)&mode=ro", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db2.Close()
+
+	// Create the table using the first (writable) connection.
+	_, err = db1.Exec(`
+		CREATE TABLE t(id INTEGER PRIMARY KEY, name TEXT);
+		INSERT INTO t(name) VALUES('alice');
+	`)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	db, err := sqlite3.OpenFlags(tmp, sqlite3.OPEN_READONLY)
+	// Select the data using the second (readonly) connection.
+	var name string
+	err = db2.QueryRow("SELECT name FROM t").Scan(&name)
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer db.Close()
+	if name != "alice" {
+		t.Errorf("got %q want alice", name)
+	}
 
-	stmt, _, err := db.Prepare(`SELECT * FROM sqlite_master`)
+	// Update table.
+	_, err = db1.Exec(`
+		DELETE FROM t;
+		INSERT INTO t(name) VALUES('bob');
+	`)
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer stmt.Close()
 
-	if stmt.Step() {
-		t.Error("want no rows")
+	// Select the data using the second (readonly) connection.
+	err = db2.QueryRow("SELECT name FROM t").Scan(&name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if name != "bob" {
+		t.Errorf("got %q want bob", name)
 	}
 }
 
