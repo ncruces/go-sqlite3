ced77aceef23fc3bd1f1b6fdccc0196616914c55
Nuno Cruces <ncruces@users.noreply.github.com>
2024-06-17 16:30:23 +0100

    Use fork.

--- util/vtabutil/parse/download.sh
+++ util/vtabutil/parse/download.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 cd -P -- "$(dirname -- "$0")"
 
-curl -#OL "https://github.com/marcobambini/sqlite-createtable-parser/raw/master/sql3parse_table.c"
-curl -#OL "https://github.com/marcobambini/sqlite-createtable-parser/raw/master/sql3parse_table.h"
+curl -#OL "https://github.com/ncruces/sqlite-createtable-parser/raw/master/sql3parse_table.c"
+curl -#OL "https://github.com/ncruces/sqlite-createtable-parser/raw/master/sql3parse_table.h"
