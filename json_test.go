6c12a8c1fa276196cd0a3e69084c87374729270c
Daenney <daenney@users.noreply.github.com>
2024-08-01 00:31:56 +0200

    More JSON/B examples. (#127)

--- json_test.go
+++ json_test.go
@@ -1,23 +1,22 @@
-package driver_test
+package sqlite3_test
 
 import (
 	"fmt"
 	"log"
 
 	"github.com/ncruces/go-sqlite3"
-	"github.com/ncruces/go-sqlite3/driver"
 	_ "github.com/ncruces/go-sqlite3/embed"
 	_ "github.com/ncruces/go-sqlite3/vfs/memdb"
 )
 
 func Example_json() {
-	db, err := driver.Open("file:/test.db?vfs=memdb")
+	db, err := sqlite3.Open("file:/test.db?vfs=memdb")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	_, err = db.Exec(`
+	err = db.Exec(`
 		CREATE TABLE orders (
 			cart_id INTEGER PRIMARY KEY,
 			user_id INTEGER NOT NULL,
@@ -40,41 +39,70 @@ func Example_json() {
 	}
 
 	// convert to JSONB on insertion
-	_, err = db.Exec(`INSERT INTO orders (user_id, cart) VALUES (?, jsonb(?))`, 123, sqlite3.JSON(Cart{
+	stmt, _, err := db.Prepare(`INSERT INTO orders (user_id, cart) VALUES (?, jsonb(?))`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	if err := stmt.BindInt(1, 123); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := stmt.BindJSON(2, Cart{
 		[]CartItem{
 			{ItemID: "111", Name: "T-shirt", Quantity: 1, Price: 250},
 			{ItemID: "222", Name: "Trousers", Quantity: 1, Price: 600},
 		},
-	}))
-	if err != nil {
+	}); err != nil {
 		log.Fatal(err)
 	}
 
-	var total string
-	err = db.QueryRow(`
+	if err := stmt.Exec(); err != nil {
+		log.Fatal(err)
+	}
+
+	sl1, _, err := db.Prepare(`
 		SELECT total(json_each.value -> 'price')
 		FROM orders, json_each(cart -> 'items')
 		WHERE cart_id = last_insert_rowid()
-	`).Scan(&total)
+	`)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer sl1.Close()
 
-	fmt.Println("total:", total)
+	for sl1.Step() {
+		fmt.Println("total:", sl1.ColumnInt(0))
+	}
 
-	var cart Cart
-	err = db.QueryRow(`
+	if err := sl1.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	sl2, _, err := db.Prepare(`
 		SELECT json(cart) -- convert to JSON on retrieval
 		FROM orders
 		WHERE cart_id = last_insert_rowid()
-	`).Scan(sqlite3.JSON(&cart))
+	`)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer sl2.Close()
 
-	for _, item := range cart.Items {
-		fmt.Printf("id: %s, name: %s, quantity: %d, price: %d\n",
-			item.ItemID, item.Name, item.Quantity, item.Price)
+	for sl2.Step() {
+		var cart Cart
+		if err := sl2.ColumnJSON(0, &cart); err != nil {
+			log.Fatal(err)
+		}
+		for _, item := range cart.Items {
+			fmt.Printf("id: %s, name: %s, quantity: %d, price: %d\n",
+				item.ItemID, item.Name, item.Quantity, item.Price)
+		}
+	}
+
+	if err := sl2.Err(); err != nil {
+		log.Fatal(err)
 	}
 
 	// Output:
