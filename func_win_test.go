faea618f08df34d78c18ab2e46094b8915b59e5e
Nuno Cruces <ncruces@users.noreply.github.com>
2025-02-22 09:26:57 +0000

    Move to ext.

--- func_win_test.go
+++ func_win_test.go
@@ -2,12 +2,9 @@ package sqlite3_test
 
 import (
 	"fmt"
-	"iter"
 	"log"
-	"slices"
 	"unicode"
 
-	"github.com/ncruces/decimal"
 	"github.com/ncruces/go-sqlite3"
 	_ "github.com/ncruces/go-sqlite3/embed"
 )
@@ -87,53 +84,4 @@ func (f *countASCII) isASCII(arg sqlite3.Value) bool {
 		}
 	}
 	return true
-}
-
-func ExampleConn_CreateAggregateFunction() {
-	db, err := sqlite3.Open(":memory:")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	err = db.Exec(`CREATE TABLE test (col)`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = db.Exec(`INSERT INTO test VALUES (0.1), (0.1), (0.1)`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = db.CreateAggregateFunction("decimal_agg", 1, sqlite3.DETERMINISTIC|sqlite3.INNOCUOUS,
-		func(s iter.Seq[[]sqlite3.Value]) any {
-			var lst []decimal.Number
-			lst = slices.AppendSeq(lst, func(yield func(decimal.Number) bool) {
-				for arg := range s {
-					if !yield(decimal.Float64(arg[0].Float())) {
-						break
-					}
-				}
-			})
-			return decimal.Sum(lst...).String()
-		})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	stmt, _, err := db.Prepare(`SELECT decimal_agg(col) FROM test`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	for stmt.Step() {
-		fmt.Println(stmt.ColumnText(0))
-	}
-	if err := stmt.Err(); err != nil {
-		log.Fatal(err)
-	}
-	// Output:
-	// 0.3000000000000000166533453693773481063544750213623046875
 }
