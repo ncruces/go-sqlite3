97091ed649f30d531f71e8c89db348d26956f953
Nuno Cruces <ncruces@users.noreply.github.com>
2025-10-06 12:36:06 +0100

    Improved replica polling.

--- litestream/vfs.go
+++ litestream/vfs.go
@@ -3,7 +3,9 @@ package litestream
 import (
 	"context"
 	"encoding/binary"
+	"errors"
 	"fmt"
+	"io"
 	"log/slog"
 	"time"
 
@@ -14,19 +16,17 @@ import (
 	"github.com/superfly/ltx"
 )
 
-const (
-	DefaultPollInterval = 1 * time.Second
-)
+const DefaultPollInterval = 1 * time.Second
 
 // VFS implements the SQLite VFS interface for Litestream.
 // It is intended to be used for read replicas that read directly from S3.
 type VFS struct {
-	client litestream.ReplicaClient
-	logger *slog.Logger
-
 	// PollInterval is the interval at which to poll the replica client for new
 	// LTX files. The index will be fetched for the new files automatically.
 	PollInterval time.Duration
+
+	client litestream.ReplicaClient
+	logger *slog.Logger
 }
 
 var _ vfs.VFS = &VFS{}
@@ -40,8 +40,6 @@ func NewVFS(client litestream.ReplicaClient, logger *slog.Logger) *VFS {
 }
 
 func (fs *VFS) Open(name string, flags vfs.OpenFlag) (vfs.File, vfs.OpenFlag, error) {
-	fs.logger.Info("opening file", "name", name, "flags", flags)
-
 	// Temp journals, as used by the sorter, use SliceFile.
 	if flags&vfs.OPEN_TEMP_JOURNAL != 0 {
 		return &vfsutil.SliceFile{}, flags | vfs.OPEN_MEMORY, nil
@@ -51,20 +49,19 @@ func (fs *VFS) Open(name string, flags vfs.OpenFlag) (vfs.File, vfs.OpenFlag, er
 		return nil, flags, sqlite3.CANTOPEN
 	}
 
-	// TODO: Clone client w/ new path based on name.
-
 	f := liteFile{
 		client:       fs.client,
 		name:         name,
-		index:        make(map[uint32]ltx.PageIndexElem),
+		pages:        map[uint32]ltx.PageIndexElem{},
+		levels:       map[int]ltx.TXID{},
 		logger:       fs.logger.With("name", name),
 		pollInterval: fs.PollInterval,
 	}
 
 	// Build the page index so we can lookup individual pages.
 	if err := f.buildIndex(context.Background()); err != nil {
-		f.logger.Error("cannot build index", "error", err)
-		return nil, 0, fmt.Errorf("cannot build index: %w", err)
+		f.logger.Error("build index", "error", err)
+		return nil, 0, err
 	}
 	return &f, flags | vfs.OPEN_READONLY, nil
 }
@@ -87,11 +84,11 @@ type liteFile struct {
 	client litestream.ReplicaClient
 	name   string
 
-	index  map[uint32]ltx.PageIndexElem
+	pages  map[uint32]ltx.PageIndexElem
+	levels map[int]ltx.TXID
 	logger *slog.Logger
 	conn   *sqlite3.Conn
 
-	pos          ltx.Pos
 	pageSize     uint32
 	pageCount    uint32
 	changeCount  uint32
@@ -99,23 +96,17 @@ type liteFile struct {
 	pollInterval time.Duration
 }
 
-func (f *liteFile) Close() error {
-	f.logger.Info("closing file")
-	return nil
-}
+func (f *liteFile) Close() error { return nil }
 
 func (f *liteFile) ReadAt(p []byte, off int64) (n int, err error) {
-	f.logger.Info("reading at", "off", off, "len", len(p))
-
 	pgno := uint32(1)
 	if off >= 512 {
 		pgno += uint32(off / int64(f.pageSize))
 	}
 
-	elem, ok := f.index[pgno]
+	elem, ok := f.pages[pgno]
 	if !ok {
-		f.logger.Error("page not found", "page", pgno)
-		return 0, sqlite3.IOERR_READ
+		return 0, io.EOF
 	}
 
 	ctx := context.Background()
@@ -125,8 +116,8 @@ func (f *liteFile) ReadAt(p []byte, off int64) (n int, err error) {
 
 	_, data, err := litestream.FetchPage(ctx, f.client, elem.Level, elem.MinTXID, elem.MaxTXID, elem.Offset, elem.Size)
 	if err != nil {
-		f.logger.Error("cannot fetch page", "error", err)
-		return 0, sqlite3.IOERR_READ
+		f.logger.Error("fetch page", "error", err)
+		return 0, err
 	}
 
 	// Update the first page to pretend we are in journal mode,
@@ -140,8 +131,6 @@ func (f *liteFile) ReadAt(p []byte, off int64) (n int, err error) {
 	}
 
 	n = copy(p, data[off%int64(len(data)):])
-	f.logger.Info("data read", "n", n, "data", len(data))
-
 	return n, nil
 }
 
@@ -162,12 +151,10 @@ func (f *liteFile) Sync(flag vfs.SyncFlag) error {
 
 func (f *liteFile) Size() (size int64, err error) {
 	size = int64(f.pageCount) * int64(f.pageSize)
-	f.logger.Info("file size", "size", size)
-	return size, nil
+	return
 }
 
 func (f *liteFile) Lock(lock vfs.LockLevel) error {
-	f.logger.Info("locking file", "lock", lock)
 	if lock >= vfs.LOCK_RESERVED {
 		return sqlite3.IOERR_LOCK
 	}
@@ -175,7 +162,6 @@ func (f *liteFile) Lock(lock vfs.LockLevel) error {
 }
 
 func (f *liteFile) Unlock(lock vfs.LockLevel) error {
-	f.logger.Info("unlocking file", "lock", lock)
 	return nil
 }
 
@@ -198,6 +184,32 @@ func (f *liteFile) SetDB(conn any) {
 	f.conn = conn.(*sqlite3.Conn)
 }
 
+func (f *liteFile) buildIndex(ctx context.Context) error {
+	infos, err := litestream.CalcRestorePlan(ctx, f.client, 0, time.Time{}, f.logger)
+	if err != nil {
+		if !errors.Is(err, litestream.ErrTxNotAvailable) {
+			return fmt.Errorf("calc restore plan: %w", err)
+		}
+		return nil
+	}
+
+	for _, info := range infos {
+		// Read page index.
+		idx, err := litestream.FetchPageIndex(ctx, f.client, info)
+		if err != nil {
+			return fmt.Errorf("fetch page index: %w", err)
+		}
+
+		// Replace pages in overall index with new pages.
+		for k, v := range idx {
+			f.pageCount = max(f.pageCount, k)
+			f.pages[k] = v
+		}
+		f.levels[info.Level] = info.MaxTXID
+	}
+	return nil
+}
+
 // pollReplicaClient fetches new LTX files from the replica client and updates
 // the page index & the current position.
 func (f *liteFile) pollReplicaClient() error {
@@ -212,52 +224,46 @@ func (f *liteFile) pollReplicaClient() error {
 		ctx = f.conn.GetInterrupt()
 	}
 
-	f.logger.Debug("polling replica client", "txid", f.pos.TXID.String())
+	for level := range litestream.SnapshotLevel + 1 {
+		err := func() error {
+			nextTXID := f.levels[level] + 1
 
-	// Start reading from the next LTX file after the current position.
-	itr, err := f.client.LTXFiles(ctx, 0, f.pos.TXID+1)
-	if err != nil {
-		return fmt.Errorf("ltx files: %w", err)
-	}
-	if !itr.Next() {
-		return nil // No changes.
-	}
+			// Start reading from the next LTX file after the current position.
+			itr, err := f.client.LTXFiles(ctx, level, nextTXID)
+			if err != nil {
+				return fmt.Errorf("ltx files: %w", err)
+			}
+			defer itr.Close()
 
-	err = f.buildIndex(ctx)
-	if err == nil {
-		f.changeCount++
-	}
-	return err
-}
+			// Build an update across all new LTX files.
+			for itr.Next() {
+				info := itr.Item()
 
-func (f *liteFile) buildIndex(ctx context.Context) error {
-	infos, err := litestream.CalcRestorePlan(ctx, f.client, 0, time.Time{}, f.logger)
-	if err != nil {
-		return fmt.Errorf("cannot calc restore plan: %w", err)
-	} else if len(infos) == 0 {
-		return fmt.Errorf("no backup files available") // TODO: Open even when no files available.
-	}
+				// Ensure we are fetching the next transaction from our current position.
+				if nextTXID != 1 && nextTXID != info.MinTXID {
+					return fmt.Errorf("non-contiguous ltx file: current=%s, next=%s-%s", nextTXID, info.MinTXID, info.MaxTXID)
+				}
 
-	// Determine the current position based off the latest LTX file.
-	if len(infos) > 0 {
-		f.pos = ltx.Pos{TXID: infos[len(infos)-1].MaxTXID}
-	}
+				// Read page index.
+				idx, err := litestream.FetchPageIndex(ctx, f.client, info)
+				if err != nil {
+					return fmt.Errorf("fetch page index: %w", err)
+				}
 
-	clear(f.index)
-	for _, info := range infos {
-		f.logger.Debug("opening page index", "level", info.Level, "min", info.MinTXID, "max", info.MaxTXID)
+				// Update the page index & current position.
+				for k, v := range idx {
+					f.pageCount = max(f.pageCount, k)
+					f.pages[k] = v
+				}
+				f.levels[level] = info.MaxTXID
+				f.changeCount++
+			}
 
-		// Read page index.
-		idx, err := litestream.FetchPageIndex(ctx, f.client, info)
+			return itr.Close()
+		}()
 		if err != nil {
-			return fmt.Errorf("fetch page index: %w", err)
-		}
-
-		// Replace pages in overall index with new pages.
-		for k, v := range idx {
-			f.logger.Debug("adding page index", "page", k, "elem", v)
-			f.pageCount = max(f.pageCount, k)
-			f.index[k] = v
+			f.logger.Error("cannot poll replica", "error", err)
+			return err
 		}
 	}
 	return nil
