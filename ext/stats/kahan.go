30649fa50a109d3d7f9e3e0c4ff6a9d0f05191c8
Nuno Cruces <ncruces@users.noreply.github.com>
2026-03-05 16:33:57 +0000

    Noncombining.

--- ext/stats/kahan.go
+++ ext/stats/kahan.go
@@ -5,15 +5,15 @@ package stats
 type kahan struct{ hi, lo float64 }
 
 func (k *kahan) add(x float64) {
-	y := k.lo + x
-	t := k.hi + y
-	k.lo = y - (t - k.hi)
+	y := float64(k.lo + x)
+	t := float64(k.hi + y)
+	k.lo = y - float64(t-k.hi)
 	k.hi = t
 }
 
 func (k *kahan) sub(x float64) {
-	y := k.lo - x
-	t := k.hi + y
-	k.lo = y - (t - k.hi)
+	y := float64(k.lo - x)
+	t := float64(k.hi + y)
+	k.lo = y - float64(t-k.hi)
 	k.hi = t
 }
